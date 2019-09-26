1a438aac79e1160eceeb78c0291387c356eb480d
Saulo Felix <meetnapoleon@protonmail.ch>
2019-09-26 10:36:09 -0300

    Added control input module

---
+++ control_input_module.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:        Universidade Federal de Sergipe
+// Engineer:       
+// 
+// Create Date:    10:22:52 09/26/2019 
+// Design Name:    Saulo G. Felix
+// Module Name:    control_input_module 
+// Project Name:   elevetor_controller
+// Target Devices: Spartan3E
+// Tool versions:  ISE 14.7
+// Description:    The control system module
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module control_input_module(
+    );
+
+
+endmodule
