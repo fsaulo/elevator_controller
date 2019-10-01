c1248e411a0cf5fe0178dd1ef83ff9a5a21acad7
Saulo G. Felix <meetnapoleon@protonmail.ch>
2019-10-01 03:30:57 -0300

    Added FIFO queue module

---
+++ fifo_memory_unit.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+///////////////////////////////////////////////////////////////////////////////
+// Institution:    Universidade Federal de Sergipe
+// Engineer:       Saulo G. Felix
+//
+// Create Date:    02:54:35 09/30/2019
+// Design Name:    Saulo G. Felix
+// Module Name:    fifo_memory_unit
+// Project Name:   elevetor_controller
+// Target Devices: Spartan3E
+// Tool versions:  ISE 14.7
+// Description:    FIFO Memory queue
+///////////////////////////////////////////////////////////////////////////////
+module fifo_memory_unit(
+    );
+
+endmodule
